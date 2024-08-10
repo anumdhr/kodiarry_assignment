49fac8f361475636ca00ac4ba0c522e57eba8243
Anu Manandhar <anumdr3@gmail.com>
2024-08-10 14:52:43 +0545

    first commit

--- lib/modules/google_maps/google_map.dart
+++ lib/modules/google_maps/google_map.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -15,16 +14,16 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(27.6618319, 85.3022104); // San Francisco
+  final LatLng _center = const LatLng(27.6318496, 85.511409);
 
   final Set<Polygon> _polygons = {
     Polygon(
       polygonId: PolygonId('polygon1'),
       points: [
-        LatLng(27.6618319, 85.3022104),
-        LatLng(27.6572758, 85.3014879),
-        LatLng(27.6562571, 85.3104714),
-        LatLng(27.6550474, 85.3161921),
+        LatLng(27.6318496, 85.511409),
+        LatLng(27.6295633, 85.5117358),
+        LatLng(27.6275661, 85.5250957),
+        LatLng(27.6334207, 85.5190829),
       ],
       strokeColor: Colors.blue,
       strokeWidth: 3,
@@ -53,43 +52,3 @@ class _MapPageState extends State<MapPage> {
     );
   }
 }
-
-
-
-
-
-
-
-// class MapPage extends StatefulWidget {
-//   @override
-//   _MapPageState createState() => _MapPageState();
-// }
-//
-// class _MapPageState extends State<MapPage> {
-//   Completer<GoogleMapController> _controller = Completer();
-//
-//   final LatLng _center = const LatLng(27.6332, 85.5277); // San Francisco
-//
-//
-//
-//   void _onMapCreated(GoogleMapController controller) {
-//     _controller.complete(controller);
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Google Maps with Polygon'),
-//       ),
-//       body: GoogleMap(
-//         onMapCreated: _onMapCreated,
-//         initialCameraPosition: CameraPosition(
-//           target: _center,
-//           zoom: 14.0,
-//         ),
-//
-//       ),
-//     );
-//   }
-// }
